4324dd4413301f0c309aa447fa05e97e6625c7c3
kewinzaq1 <kewinszlezingier@gmail.com>
2022-07-12 08:06:21 +0200

    add config files to git

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <JokeWrapper/>
+</template>
+
+<script lang="js">
+import
+  JokeWrapper
+  from '~/components/JokeWrapper';
+
+export default {
+  name: 'IndexPage',
+  components: {JokeWrapper},
+  inject: ['$chakraColorMode', '$toggleColorMode'],
+  data() {
+    return {}
+  },
+}
+</script>
