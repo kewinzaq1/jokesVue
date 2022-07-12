36ea44ea84d827cd0fea133e44d52d9992e8762b
kewinzaq1 <kewinszlezingier@gmail.com>
2022-07-12 08:05:33 +0200

    move wrapper to JokeWrapper

---
+++ components/JokeWrapper.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <CFlex
+      display="flex" align-items="center" justify-content="center" flex-direction="column" height="100vh"
+      width="50%" mx="auto">
+      <CHeading as="h1" font-size='3rem' font-weight="400">Jokes</CHeading>
+      <CText font-size="1.25rem">Choose category and display random joke</CText>
+      <Select/>
+      <JokeModal/>
+    </CFlex>
+  </div>
+</template>
+
+<script>
+import {CFlex, CHeading, CText} from "@chakra-ui/vue";
+
+
+export default {
+  name: "JokeWrapper",
+  components: {
+    CText, CFlex, CHeading
+  }
+}
+</script>
+
+
